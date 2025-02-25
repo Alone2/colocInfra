cb54e709eca4ba3408bc95ae36d90a3fcae35eae
Alain Sinzig <alain.sinzig@epfl.ch>
2025-02-25 02:15:47 +0100

    setup.sh: update

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 domain_args="-d wg.a1n.ch -d split.wg.a1n.ch -d chores.wg.a1n.ch"
 
-cp ./config/nginx_active/nginx_certbot.conf ./config/nginx_active/nginx.conf
+cp ./config/nginx/nginx_certbot.conf ./config/nginx/nginx.conf
 
-docker compose up -d 
-docker compose restart nginx
+podman-compose up -d 
+podman-compose restart nginx
 
-docker compose exec certbot certbot certonly --webroot -w /var/www/certbot $domain_args
+podman-compose exec certbot certbot certonly --webroot -w /var/www/certbot $domain_args
 
-cp ./config/nginx_active/nginx_default.conf ./config/nginx_active/nginx.conf
+cp ./config/nginx/nginx_default.conf ./config/nginx/nginx.conf
 
-docker compose restart nginx
-docker compose up -d
+podman-compose restart nginx
+podman-compose up -d
